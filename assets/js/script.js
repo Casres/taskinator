271de914b3161f866c5e649bfdb030d9957fae2c
Casres <christian@casillas.tech>
2022-03-18 16:24:46 -0400

    making a ssave point

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,20 @@
+
+
 var buttonEl = document.querySelector("#save-task");
 
-console.log(buttonEl);
-
-buttonEl.addEventListener("click", function(){
-    // alert("button clicked");
-    addTaskBtn();
-});
-
-
 function addTaskBtn() {
-
+    
     var taskEL = document.querySelector("#list-items");
     var newTaskEl = document.createElement("li"); 
-    // var userPrompt = window.prompt("what's your task?")
-
-    // newTaskEl.userPrompt.textcontent(userPrompt);
     
+    newTaskEl.textContent = "New Task";
     newTaskEl.className = "list-item-groups"; 
     taskEL.appendChild(newTaskEl);
-
-
+    
 }
+
+buttonEl.addEventListener("click", function(){
+    
+    addTaskBtn();
+
+});
